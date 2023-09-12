ff1e45280d2ee3aad84b646f0887de69649e2880
Sukrija Kararic <sukrijakararic1@gmail.com>
2023-09-12 11:01:48 -0400

    added htm and css

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-// Arrays that will be randomly selected
+// Arrays that hold the ingredients for a milkhake
 let toppings = ['chocolate chips', 'M&M`s', 'oreo', 'cookie dough', 'sprinkles']
 let iceCream = ['chocolate', 'vanilla', 'strawberry','raspberry', 'coconut']
 let syrup = ['chocolate', 'hazelnut', 'strawberry', 'cookies n` cream']
 
 
-// Code that will randomly seclect certain items from the array above
+// These variables grab the HTNL elements needed to interact with the DOM
+const shek = document.getElementById("urshake");
+const whip = document.getElementById("myshake");
+
+// This function will be called when the button is clicked
+
+function yourShake () {
+// This will grab a random element from each array
 const randTopping = toppings[Math.floor(Math.random() * toppings.length)];
 const randIceCream = iceCream[Math.floor(Math.random() * iceCream.length)];
 const randSyrup = syrup[Math.floor(Math.random() * syrup.length)];
-
 //This will print a random milkshake using the ingredients from the Arrays.
-console.log(`I think today i'll have a ${randIceCream} ice cream with ${randTopping} topping and ${randSyrup} syrup!`);
+whip.innerHTML = `I think today i'll have a ${randIceCream} ice cream with ${randTopping} topping and ${randSyrup} syrup milkshake!`;
+
+// After the button is initially clicked, the trext inside will chanmge to this.
+document.querySelector('button').innerHTML = 'Get another milkshake!'
+}
+
+// When the button is clicked, the function above will run
+shek.onclick = yourShake;
